e699a0bcfad8d68b8a62c9764bef3e06cdae62b2
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-11-26 15:06:01 -0500

    pre-deploy commit

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -5,7 +5,14 @@ import SEO from "../components/seo"
 import { Badge, Card, CardBody, CardSubtitle } from "reactstrap"
 import Img from "gatsby-image"
 import { slugify } from "../util/utilityFunctions"
-import authors from '../util/authors'
+import authors from "../util/authors"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebook,
+  faTwitter,
+  faGoogle,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons"
 
 export const postQuery = graphql`
   query blogPostBySlug($slug: String!, $imageUrl: String!) {
@@ -26,8 +33,8 @@ export const postQuery = graphql`
         }
       }
     }
-    file(relativePath: {eq: $imageUrl}){
-      childImageSharp{
+    file(relativePath: { eq: $imageUrl }) {
+      childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid
         }
@@ -36,37 +43,87 @@ export const postQuery = graphql`
   }
 `
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
   const post = data.markdownRemark.frontmatter
   const author = authors.find(x => x.name === post.author)
   const { title, date, tags } = post
+  const baseUrl = "https://gatsbytutorial.co.uk/"
   return (
-    <Layout pageTitle={title} postAuthor={author} authorImageFluid={data.file.childImageSharp.fluid}>
+    <Layout
+      pageTitle={title}
+      postAuthor={author}
+      authorImageFluid={data.file.childImageSharp.fluid}
+    >
       <SEO title={title} />
-          <Card>
-            <Img
-              className="card-image-top"
-              fluid={post.image.childImageSharp.fluid}
-            />
-            <CardBody>
-              <CardSubtitle>
-                <span className="text-info">{date}</span> by{" "}
-                <span className="text-info">{author}</span>
-              </CardSubtitle>
-              <div
-                dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-              ></div>
-              <ul className="post-tags">
-                {tags.map(tag => (
-                  <li key={tag}>
-                    <Link to={`/tag/${slugify(tag)}`}>
-                      <Badge color="primary">{tag}</Badge>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </CardBody>
-          </Card>
+      <Card>
+        <Img
+          className="card-image-top"
+          fluid={post.image.childImageSharp.fluid}
+        />
+        <CardBody>
+          <CardSubtitle>
+            <span className="text-info">{date}</span> by{" "}
+            <span className="text-info">{author.name}</span>
+          </CardSubtitle>
+          <div
+            dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+          ></div>
+          <ul className="post-tags">
+            {tags.map(tag => (
+              <li key={tag}>
+                <Link to={`/tag/${slugify(tag)}`}>
+                  <Badge color="primary">{tag}</Badge>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </CardBody>
+      </Card>
+      <h3 className="text-center">Share this post</h3>
+      <div className="text-center social-share-links">
+        <ul>
+          <li>
+            <a
+              href={`https://www.facebook.com/sharer/sharer.php?u=${baseUrl}${pageContext.slug}`}
+              className="facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+          </li>
+          <li>
+            <a
+              href={`https://twitter.com/share?url=${baseUrl}${pageContext.slug}&text=${post.title}&viaSamThoyre`}
+              className="twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </li>
+          <li>
+            <a
+              href={`https://plus.google.com/share?url=${baseUrl}${pageContext.slug}`}
+              className="google"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGoogle} size="2x" />
+            </a>
+          </li>
+          <li>
+            <a
+              href={`https://www.linkedin.com/shareArticle?url=${baseUrl}${pageContext.slug}`}
+              className="linkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </Layout>
   )
 }
